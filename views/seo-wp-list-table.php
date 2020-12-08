a5c44d6101ac8e39864806dbb8d735bc85eb6818
DanielC <danielcordero1998@gmail.com>
2020-12-07 23:09:58 -0400

    [add]:add search box (bug with table )

--- views/seo-wp-list-table.php
+++ views/seo-wp-list-table.php
@@ -1,6 +1,6 @@
 <?php
 if(!class_exists('Lib')){
-    require_once( ABSPATH . 'wp-content/plugins/wp-list-table/Lib.php');
+    require_once( ABSPATH . 'wp-content/plugins/wp_list_table/Lib.php');
 }
 
 class WPListTable extends Lib{
@@ -12,7 +12,9 @@ class WPListTable extends Lib{
         $orderby = isset($_GET['orderby']) ? trim($_GET['orderby']) : "" ;
         $order   = isset($_GET['order'])   ? trim($_GET['order'])   : "" ;
 
-        $this->items = $this->wp_list_table_data($orderby,$order);
+        $search_term = isset($_POST['s'])  ? trim($_POST['s']) : "";
+
+        $this->items = $this->wp_list_table_data($orderby,$order,$search_term);
 
         $columns = $this->get_columns();
         $hidden = $this->get_hidden_columns();
@@ -22,19 +24,34 @@ class WPListTable extends Lib{
 
     }
     //
-    public function wp_list_table_data($orderby='' , $order=''){
+    public function wp_list_table_data($orderby='' , $order='', $search_term=''){
 
         global $wpdb;
         
-        $args = array(
-            'numberposts' => -1,
-            'suppress_filters' => false
-        );
-        // $all_posts = get_post($args);
-        $all_posts = $wpdb->get_results(
-            'SELECT * FROM '. $wpdb->posts
-        );
-
+        // $args = array(
+        //     'numberposts' => -1,
+        //     'suppress_filters' => false
+        // );
+        // $all_posts2 = get_post($args);
+        if( !empty($search_term) ){
+            echo 'buscar';
+            $all_posts = $wpdb->get_results(
+               'SELECT * FROM ' . $wpdb->posts . " WHERE post_type = 'post' AND post_status = 'publish' AND (post_title LIKE '%$search_term%' OR post_content LIKE '%$search_term%')"
+            );
+        }else{
+            echo 'no buscar';
+            if($orderby=='title' && $order=='desc'){
+                $all_posts = $wpdb->get_results(
+                    'SELECT * FROM '. $wpdb->posts . " WHERE post_type = 'post' AND post_status='publish' ORDER BY post_title DESC"
+                );
+            }else{
+                $all_posts = $wpdb->get_results(
+                    'SELECT * FROM '. $wpdb->posts . " WHERE post_type = 'post' AND post_status='publish' ORDER BY post_title ASC"
+                );
+            }
+       
+        }
+        
         
         $post_array = array();
        
@@ -44,7 +61,7 @@ class WPListTable extends Lib{
                 $post_array[]= array(
                     'id' => $post->ID,
                     'title'=> $post->post_title,
-                    'content'=> $post->post_content,
+                    //'content'=> $post->post_content,
                     'slug'=> $post->post_name
                 );
             }
@@ -60,7 +77,7 @@ class WPListTable extends Lib{
     public function get_sortable_columns(){
         
         return array(
-            
+            'title' => array('title',true)
         );
 
     }
@@ -70,7 +87,7 @@ class WPListTable extends Lib{
         $columns = array(
             'id' => 'ID',
             'title' => 'Title',
-            'content' => 'Content',
+            // 'content' => 'Content',
             'slug' => 'Post Slug',
         );
 
@@ -85,7 +102,7 @@ class WPListTable extends Lib{
             case 'slug':
                 return $item[$column_name];
             default:
-                return print_r($item , true);
+                 return print_r($item , true);
         }
     }
    
@@ -95,7 +112,12 @@ function show_data_wp_list_table(){
     $wp_table = new WPListTable();
 
     $wp_table->prepare_items();
+ 
     echo  '<h3>Table list</h3>' ;
+    
+    echo "<form method='post' name='frm_search_post' action='".$_SERVER['PHP_SELF']."?page=list-table' >";
+    $wp_table->search_box('Search post','search_post_id');
+    echo '</form>';
     $wp_table->display();
 
 }
